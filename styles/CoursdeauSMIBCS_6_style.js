3df42c5f9986f9746a9b2f66b2f5596722e0bb0b
SMIBCS <105497206+SMIBCS@users.noreply.github.com>
2022-05-13 14:58:40 +0200

    Add files via upload

---
+++ styles/CoursdeauSMIBCS_6_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_CoursdeauSMIBCS_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.4px \'Cambria\', sans-serif";
+    var labelFill = "#0091ca";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if (feature.get("TOPONYME") !== null) {
+        labelText = String(feature.get("TOPONYME"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(72,123,182,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
